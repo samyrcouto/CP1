722cc8f13d6cd9b62e7e3ce8e23729ba089201ff
samyr <costosinho50@gmail.com>
2026-03-16 11:13:17 -0300

    Feat: Adicionando useStates e TextInputs

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button} from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native/types_generated/index';
+
 
 export default function App() {
-  return (
+  const [nome, setNome] = useState("")
+  const [curso, setCurso] = useState("")
+  const [disciplina, setDisciplina] = useState("")
+  const [descricao, setDescricao] = useState("")
+  const [mostrarDados, setMostrarDados] = useState(false)
+
+
+return (
+  <SafeAreaView>
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Image 
+        source={require("./assets/forms.png")}
+        style={styles.image}
+      />
+      <TextInput
+        placeholder ="Digite seu nome"
+        style={styles.input}
+        maxLength ={10}
+        autoCapatalize = "words"
+        onChangeText={setNome}
+      />
+      <TextInput
+        placeholder
+      
+      
+      />
+
+      <TextInput
+      
+      
+      />
+
+      <TextInput
+      
+      
+      />
+        
+
+      
+   
+    <Button
+      title="Envio dos dados"
+    />
+    
+  
+    
+
+    
+      
+      </View>
+  </SafeAreaView>
   );
 }
 
@@ -15,6 +65,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:200,
+    gap:10
   },
+  image:{
+    resizeMode:"center"
+  },
+  input:{
+    backgroundColor:"#ccc",
+    width:300,
+    borderRadius:10
+  }
 });
